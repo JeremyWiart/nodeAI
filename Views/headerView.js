50a3dcb78ff4d69907ab7d441977fb8fc4a4e339
Jeremy <jeremy.wiart@outlook.com>
2024-12-17 14:10:19 +0100

    ajout banniere top,header

--- Views/headerView.js
+++ Views/headerView.js
@@ -1,5 +1,11 @@
 function headerView(){
-    return `<header></header>`;
+    return `<header class="border-2 border-black">
+                <div class="flex justify-center items-center w-6/6 border-2 border-black">
+                    <aside>
+                        <img src="../img/top_bann.png" alt="topban">
+                    </aside>
+                </div>
+            </header>`;
 
 }
 
