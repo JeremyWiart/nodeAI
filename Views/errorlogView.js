08df749012d86fff4244b9eb6ceefb04173df30d
Jeremy <jeremy.wiart@outlook.com>
2024-12-19 13:32:32 +0100

    css sur l'erreur

--- Views/errorlogView.js
+++ Views/errorlogView.js
@@ -1,6 +1,6 @@
 function errorlogView(){
 
-    return `<div>Wrong password or username</div>`;
+    return `<p class="text-red-600 text-center border-2 border-red-600 rounded-lg">Wrong password or username</p>`;
 }
 
 module.exports = errorlogView;
